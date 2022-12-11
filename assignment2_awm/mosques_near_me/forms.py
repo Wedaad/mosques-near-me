abb6d55fd29ad4a3abe467572fe7e136c0f5e6f1
Wedaad <wedaadh2@gmail.com>
2022-12-11 21:03:31 +0000

    fix: register user bug

--- assignment2_awm/mosques_near_me/forms.py
+++ assignment2_awm/mosques_near_me/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
@@ -12,7 +12,7 @@ class RegisterUserForm(UserCreationForm):
         fields = ("username", "email", "password1", "password2")
 
     def save(self, commit=True):
-        user = super(RegisterUserForm, self).save(commit=True)
+        user = super(RegisterUserForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
         user.username = self.cleaned_data['username']
 
